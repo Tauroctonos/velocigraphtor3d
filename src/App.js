21d36e1cb8aaca432c19d4060c96393eae89c5d9
Stan Peck <SPeck@navexglobal.com>
2019-08-08 12:48:27 -0700

    Simplify app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Icosahedron from './Icosahedron'
 
-class App extends Component{
-  render() {
-    return (
-      <div id='sceneHolder'>
-        <Icosahedron/>
-        <Icosahedron/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div id='sceneHolder'>
+      <Icosahedron/>
+      <Icosahedron/>
+    </div>
+  );
 }
 
 export default App;
