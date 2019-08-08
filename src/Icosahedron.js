ddbb3a7c4ebdeb19b81efafcceb8de0d0f973817
Stan Peck <SPeck@navexglobal.com>
2019-08-08 14:17:59 -0700

    Pull componentDidMount apart into functions

--- src/Icosahedron.js
+++ src/Icosahedron.js
@@ -4,26 +4,54 @@ import * as THREE from "three";
 
 class Icosahedron extends Component{
   componentDidMount() {
-    // === THREE.JS CODE START ===
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
-    this.mount.appendChild( renderer.domElement );
-    var geometry = new THREE.IcosahedronGeometry ();
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var icosahedron = new THREE.Mesh( geometry, material );
-    scene.add( icosahedron );
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
-      icosahedron.rotation.x += 0.01;
-      icosahedron.rotation.y += 0.01;
-      renderer.render( scene, camera );
-    };
-    animate();
-    // === THREE.JS EXAMPLE CODE END ===
+    this.sceneSetup();
+    this.addIcosahedronToScene();
+    this.startAnimationLoop();
   }
+
+  sceneSetup = () => {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    
+    this.camera.position.z = 5;
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth/2, window.innerHeight/2 );
+    this.mount.appendChild( this.renderer.domElement );
+  };
+
+  addIcosahedronToScene = () => {
+    const geometry = new THREE.IcosahedronGeometry();
+    const material = new THREE.MeshPhongMaterial( {
+        color: 0x00ff00,
+        emissive: 0x072534,
+        side: THREE.DoubleSide,
+        flatShading: true
+    } );
+    this.icosahedron = new THREE.Mesh( geometry, material );
+    this.scene.add( this.icosahedron );
+
+    const lights = [];
+    lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+    lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+    lights[ 0 ].position.set( 0, 200, 0 );
+    lights[ 1 ].position.set( 100, 200, 100 );
+    lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+    this.scene.add( lights[ 0 ] );
+    this.scene.add( lights[ 1 ] );
+    this.scene.add( lights[ 2 ] );
+  };
+
+  startAnimationLoop = () => {
+    this.icosahedron.rotation.x += 0.01;
+    this.icosahedron.rotation.y += 0.01;
+
+    this.renderer.render( this.scene, this.camera );
+    this.requestID = window.requestAnimationFrame(this.startAnimationLoop);
+  };
 
   render() {
     return (
